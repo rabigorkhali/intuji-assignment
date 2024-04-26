9461b3c2b6a78cf7b7c5c1793f7e4927d11533be
Rabi Gorkhali <rabigorkhaly@gmail.com>
2024-04-26 15:56:20 +0545

    chore: remove view files which are not needed

--- Controller/EventController.php
+++ Controller/EventController.php
@@ -1,25 +1,20 @@
 <?php
 require_once 'model/eventModel.php';
-require_once 'view/eventView.php';
 
 class eventController {
     private $eventModel;
-    private $eventView;
 
     public function __construct() {
         $this->eventModel  = new EventModel();
-        $this->eventView = new EventView();
     }
 
     public function listEvents() {
         $events = $this->eventModel->listEvents();
         return $events;
-        // $this->eventView->displayEvents($events);
     }
 
     public function createEvent($eventData) {
         $this->eventModel->createEvent($eventData);
-        // Redirect back to index.php after event creation
         header('Location: index.php');
         exit();
     }
